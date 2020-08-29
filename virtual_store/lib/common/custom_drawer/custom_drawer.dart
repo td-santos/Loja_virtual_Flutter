e8d37f50511ea005fb64317354d7582cb7238dde
Thiago Dantas <thiago_dantas411@hotmail.com>
2020-08-28 22:45:30 -0300

    commit block fields
    - bloquando campos na tela de cadastro

--- virtual_store/lib/common/custom_drawer/custom_drawer.dart
+++ virtual_store/lib/common/custom_drawer/custom_drawer.dart
@@ -6,15 +6,31 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
+      child: Stack(
         children: <Widget>[
-          CustomDrawerHeader(),
-          DrawerTile(iconData: Icons.home, title: 'Início', page: 0,),
-          DrawerTile(iconData: Icons.list, title: 'Produtos', page: 1,),
-          DrawerTile(iconData: Icons.playlist_add_check, title: 'Meus Pedidos', page: 2,),
-          DrawerTile(iconData: Icons.location_on, title: 'Lojas', page: 3,),
-        ],
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Color.fromARGB(255, 203, 236, 241),Colors.white
+                ]
+              )
+            ),
+          ),
+          ListView(
+            children: <Widget>[
+              CustomDrawerHeader(),
+              Divider(),
+              DrawerTile(iconData: Icons.home, title: 'Início', page: 0,),
+              DrawerTile(iconData: Icons.list, title: 'Produtos', page: 1,),
+              DrawerTile(iconData: Icons.playlist_add_check, title: 'Meus Pedidos', page: 2,),
+              DrawerTile(iconData: Icons.location_on, title: 'Lojas', page: 3,),
+            ],
       ),
+        ],
+      )
     );
   }
 }
--- virtual_store/lib/screens/signup/signup_screen.dart
+++ virtual_store/lib/screens/signup/signup_screen.dart
@@ -5,11 +5,10 @@ import 'package:provider/provider.dart';
 import 'package:virtual_store/models/user_manager.dart';
 
 class SignUpScreen extends StatelessWidget {
-
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   final User user = User();
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,118 +22,127 @@ class SignUpScreen extends StatelessWidget {
           margin: EdgeInsets.symmetric(horizontal: 16),
           child: Form(
             key: formKey,
-             child: ListView(
-              shrinkWrap: true,
-              padding: EdgeInsets.all(16),
-              children: <Widget>[
-                TextFormField(
-                  decoration: InputDecoration(hintText: 'Nome Completo'),
-                  validator: (name){
-                    if(name.isEmpty){
-                      return 'Campo obrigatório';
-                    }else if(name.trim().split(' ').length <= 1){
-                      return 'Insira seu nome Completo';
-                    }else{
-                      return null;
-                    }
-                  },
-                  onSaved: (name){
-                    user.name = name;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(hintText: 'E-mail'),
-                  validator: (email){
-                    if(email.isEmpty){
-                      return 'Campo obrigatório';
-                    }else if(!emailValid(email)){
-                      return 'E-mail inválido';
-                    }else{
-                      return null;
-                    }
-                  },
-                  onSaved: (email){
-                    user.email = email;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: 'Senha'),
-                  validator: (senha){
-                    if(senha.isEmpty){
-                      return 'Campo Obrigatório';
-                    }else if(senha.length <6){
-                      return 'Senha muito curta';
-                    }else{
-                      return null;
-                    }
-                  },
-                  onSaved: (senha){
-                    user.senha = senha;
-                  },
-                ),
-                SizedBox(height: 16),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: 'Repita a Senha'),
-                  validator: (confirmSenha){
-                    if(confirmSenha.isEmpty){
-                      return 'Campo Obrigatório';
-                    }else if(confirmSenha.length <6){
-                      return 'Senha muito curta';
-                    }else{
-                      return null;
-                    }
-                  },
-                  onSaved: (confirmSenha){
-                    user.confirmSenha = confirmSenha;
-                  },
-                ),
-                SizedBox(height: 16),
-                SizedBox(height: 44,
-                  child: RaisedButton(
-                    color: Theme.of(context).primaryColor,
-                    disabledColor: Theme.of(context).primaryColor.withAlpha(80),
-                    textColor: Colors.white,
-                    child: Text('Criar Conta',style: TextStyle(fontSize: 18),),
-                    onPressed: (){
-                      if(formKey.currentState.validate()){
-                        formKey.currentState.save();
-                        if(user.senha != user.confirmSenha){
-                          scaffoldKey.currentState.showSnackBar(
-                              SnackBar(
-                                content:  const Text('Senhas não coincidem!'),
-                                backgroundColor: Colors.red,
-                              )
-                          );
-                          return;
-                        }else{
-                          context.read<UserManager>().signUp(
-
-                            user: user,                            
-                            onSuccess: (){
-                              //debugPrint('sucesso ao cadastrar');
-                              Navigator.of(context).pop();
-                            },
-                            onFail: (e){
-                              scaffoldKey.currentState.showSnackBar(
-                              SnackBar(
-                                content: Text('Falha ao cadastrar: $e'),
-                                backgroundColor: Colors.red,
-                              )
-                            );
-                            },
-                          );
-                        }
+            child: Consumer<UserManager>(builder: (_, userManager, __) {
+              return ListView(
+                shrinkWrap: true,
+                padding: EdgeInsets.all(16),
+                children: <Widget>[
+                  TextFormField(
+                    enabled: !userManager.loading,
+                    decoration: InputDecoration(hintText: 'Nome Completo'),
+                    validator: (name) {
+                      if (name.isEmpty) {
+                        return 'Campo obrigatório';
+                      } else if (name.trim().split(' ').length <= 1) {
+                        return 'Insira seu nome Completo';
+                      } else {
+                        return null;
                       }
-                    }
+                    },
+                    onSaved: (name) {
+                      user.name = name;
+                    },
                   ),
-                )
-              ],
-            ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    enabled: !userManager.loading,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(hintText: 'E-mail'),
+                    validator: (email) {
+                      if (email.isEmpty) {
+                        return 'Campo obrigatório';
+                      } else if (!emailValid(email)) {
+                        return 'E-mail inválido';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (email) {
+                      user.email = email;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    enabled: !userManager.loading,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Senha'),
+                    validator: (senha) {
+                      if (senha.isEmpty) {
+                        return 'Campo Obrigatório';
+                      } else if (senha.length < 6) {
+                        return 'Senha muito curta';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (senha) {
+                      user.senha = senha;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  TextFormField(
+                    enabled: !userManager.loading,
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: 'Repita a Senha'),
+                    validator: (confirmSenha) {
+                      if (confirmSenha.isEmpty) {
+                        return 'Campo Obrigatório';
+                      } else if (confirmSenha.length < 6) {
+                        return 'Senha muito curta';
+                      } else {
+                        return null;
+                      }
+                    },
+                    onSaved: (confirmSenha) {
+                      user.confirmSenha = confirmSenha;
+                    },
+                  ),
+                  SizedBox(height: 16),
+                  SizedBox(
+                    height: 44,
+                    child: RaisedButton(
+                        color: Theme.of(context).primaryColor,
+                        disabledColor: Theme.of(context).primaryColor.withAlpha(80),
+                        textColor: Colors.white,
+                        child: userManager.loading
+                        ? CircularProgressIndicator(
+                          strokeWidth: 1,
+                          valueColor: AlwaysStoppedAnimation(Theme.of(context).primaryColor),
+                        )
+                        : Text('Criar Conta',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        onPressed: userManager.loading ? null : () {
+                          if (formKey.currentState.validate()) {
+                            formKey.currentState.save();
+                            if (user.senha != user.confirmSenha) {
+                              scaffoldKey.currentState.showSnackBar(SnackBar(
+                                content: const Text('Senhas não coincidem!'),
+                                backgroundColor: Colors.red,
+                              ));
+                              return;
+                            } else {
+                              userManager.signUp(
+                                    user: user,
+                                    onSuccess: () {
+                                      //debugPrint('sucesso ao cadastrar');
+                                      Navigator.of(context).pop();
+                                    },
+                                    onFail: (e) {
+                                      scaffoldKey.currentState
+                                          .showSnackBar(SnackBar(
+                                        content: Text('Falha ao cadastrar: $e'),
+                                        backgroundColor: Colors.red,
+                                      ));
+                                    },
+                                  );
+                            }
+                          }
+                        }),
+                  )
+                ],
+              );
+            }),
           ),
         ),
       ),
