e91208535b50db98d14b258bd6b8530207ec1b5c
Thiago Dantas <thiago_dantas411@hotmail.com>
2020-09-13 21:53:37 -0300

    commit remove item
    - adicionado dialog com opçao de excluir item da seçao.

--- virtual_store/lib/models/section.dart
+++ virtual_store/lib/models/section.dart
@@ -32,6 +32,11 @@ class Section extends ChangeNotifier{
     notifyListeners();
   }
 
+  void removeItem(SectionItem item){
+    items.remove(item);
+    notifyListeners();
+  }
+
   String toString(){
     return  'Section{name: $name, type: $type, items: $items}';
   }
--- virtual_store/lib/screens/home/components/item_tile.dart
+++ virtual_store/lib/screens/home/components/item_tile.dart
@@ -2,7 +2,9 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:virtual_store/models/home_manager.dart';
 import 'package:virtual_store/models/product_manager.dart';
+import 'package:virtual_store/models/section.dart';
 import 'package:virtual_store/models/section_item.dart';
 import 'package:provider/provider.dart';
 
@@ -13,6 +15,9 @@ class ItemTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final homeManager = context.watch<HomeManager>();
+
     return GestureDetector(
       onTap: (){
         if(item.product != null){
@@ -22,6 +27,25 @@ class ItemTile extends StatelessWidget {
           }
         }
       },
+      onLongPress:homeManager.editing ? (){
+        showDialog(
+          context: context,
+          builder: (_){
+            return AlertDialog(
+              title: Text('Editar Item'),
+              actions: <Widget>[
+                FlatButton(
+                  onPressed: (){
+                    context.read<Section>().removeItem(item);
+                    Navigator.of(context).pop();
+                  }, 
+                  textColor: Colors.red,
+                  child: Text('Excluir'))
+              ],
+            );
+          }
+        );
+      }: null,
       child: AspectRatio(
         aspectRatio: 1,
         child: item.image is String
