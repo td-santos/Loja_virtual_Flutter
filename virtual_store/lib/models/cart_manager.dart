44c62576676a6005b0e673b7070e3b514482aae0
Thiago Dantas <thiago_dantas411@hotmail.com>
2020-09-22 02:02:22 -0300

    commit transform address in object
    - transformando o endereco em objeto para nao depender da api de cep utilizada.

--- virtual_store/lib/models/cart_manager.dart
+++ virtual_store/lib/models/cart_manager.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:virtual_store/models/address.dart';
 import 'package:virtual_store/models/product.dart';
 import 'package:virtual_store/models/user.dart';
 import 'package:virtual_store/models/user_manager.dart';
@@ -11,6 +12,7 @@ class CartManager extends ChangeNotifier{
 
     List<CartProduct> items =[];
     User user;
+    Address address;
     num productsPrice = 0.0;
 
     void addToCart(Product product){
@@ -97,9 +99,22 @@ class CartManager extends ChangeNotifier{
 
       try{
 
-        final address = await cepAbertoService.getAdressFromCep(cep);
+        final cepAbertoAddress = await cepAbertoService.getAdressFromCep(cep);
 
-        print(address);
+        if(cepAbertoAddress != null){
+          address = Address(
+            street: cepAbertoAddress.logradouro,
+            district: cepAbertoAddress.bairro,
+            zipCode: cepAbertoAddress.cep,
+            city: cepAbertoAddress.cidade.nome,
+            state: cepAbertoAddress.estado.sigla,
+            lat: cepAbertoAddress.latitude,
+            long: cepAbertoAddress.longitude
+          );
+        }
+
+        notifyListeners();
+        //print(cepAbertoAddress);
 
       }catch (e){
         debugPrint(e.toString());
--- virtual_store/lib/screens/address/components/address_card.dart
+++ virtual_store/lib/screens/address/components/address_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:virtual_store/models/cart_manager.dart';
 
 import 'cep_input_field.dart';
 
@@ -9,17 +11,23 @@ class AddressCard extends StatelessWidget {
       margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
       child: Padding(
         padding: EdgeInsets.fromLTRB(16, 16, 16, 4),
-        child: Form(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                'Endereço de Entrega',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+        child: Consumer<CartManager>(
+          builder: (_, cartManager, __) {
+            final address = cartManager.address;
+            print(address);
+            return Form(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Endereço de Entrega',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                  ),
+                  CepInputField(),
+                ],
               ),
-              CepInputField(),
-            ],
-          ),
+            );
+          },
         ),
       ),
     );
